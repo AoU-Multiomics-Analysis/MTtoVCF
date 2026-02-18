392aec395dfb582e0525d68f5dbd9c73f4e9c5d6
Evin Padhi <evinpadhi@gmail.com>
2026-02-17 18:50:32 -0800

    Update to add max allele count threshold

--- scripts/filter_and_write_mt.py
+++ scripts/filter_and_write_mt.py
@@ -52,7 +52,7 @@ def main(args):
     mt_filtered = mt_filtered.filter_rows(mt_filtered.info.AN >= int(args.AlleleNumberPercentage)/100 * mt_filtered.count_cols() * 2)
     
     # filter by allele count
-    mt_filtered = mt_filtered.filter_rows(hl.min(mt_filtered.info.AC) >= int(args.AlleleCount)) 
+    #mt_filtered = mt_filtered.filter_rows(hl.min(mt_filtered.info.AC) >= int(args.AlleleCount)) 
     mt_filtered = mt_filtered.filter_rows(
         (hl.min(mt_filtered.info.AC) >= int(args.MinAlleleCount)) &
         (hl.max(mt_filtered.info.AC) <= int(args.MaxAlleleCount))
